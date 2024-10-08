a6a96b17e2676b3b5b5ac45311db73259f7d2224
Borys Nadykto <borys.nadykto@bynesoft.com>
2024-10-08 19:49:38 +0100

    Heroku

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,8 +1,13 @@
 import os
 from pydantic_settings import BaseSettings
 
+def correct_postgres_url(url):
+    if url.startswith('postgres://'):
+        return 'postgresql://' + url[len('postgres://'):]
+    return url
+
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.environ["DATABASE_URL"]
+    DATABASE_URL: str = correct_postgres_url(os.environ["DATABASE_URL"])
     SECRET_KEY: str = os.environ["SECRET_KEY"]
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
