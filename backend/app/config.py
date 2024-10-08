efe1eaf0cdc770a6d45c33fe0e835223d3daadfa
Borys Nadykto <borys.nadykto@bynesoft.com>
2024-10-08 18:59:37 +0100

    Added heroku support

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,6 +8,6 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     CORS_ORIGINS: str = os.environ["CORS_ORIGINS"]
     BACKEND_HOST: str = os.environ["BACKEND_HOST"]
-    BACKEND_PORT: int = int(os.environ["BACKEND_PORT"])
+    BACKEND_PORT: int = int(os.environ.get("PORT", 8000))
 
 settings = Settings()
