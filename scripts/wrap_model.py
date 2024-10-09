55092c8311ee49cfd4791fc23d7a8aade000e80c
Borys Nadykto <borys.nadykto@bynesoft.com>
2024-10-09 11:34:19 +0100

    Added Vercel/Heroku deployment

--- scripts/wrap_model.py
+++ scripts/wrap_model.py
@@ -75,7 +75,11 @@ def get_user_inputs() -> Tuple[str, str, str, str]:
     target_repo_name = input("Enter the target repository name (e.g., 'my-logged-model'): ")
 
     # Get the byne-serve address
-    host = input(f"Enter the byne-serve address without a slash at the end (e.g., 'http://localhost:8000/reports/{target_repo_name}'): ")
+    default = f'https://byne-serve-755fc296550a.herokuapp.com/reports/{target_repo_name}'
+    host = input(f"Enter the byne-serve address without a slash at the end (default: {default}): ")
+
+    if host == "":
+        host = default
 
     return model_name_or_path, target_repo_name, host
 
